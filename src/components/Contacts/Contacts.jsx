fb6a8640459236237900f2b68c0f1bd80f62fa1a
vosob <osobskyi.vlad@gmail.com>
2024-01-28 03:52:06 +0200

    fix

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -11,6 +11,7 @@ const Contacts = () => {
 
   useEffect(() => {
     dispatch(fetchContacts());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const filter = useSelector(state => state.filter.filter);
